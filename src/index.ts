aee3063abb7cb88c1472a960bab63ef4d3bd24a1
Woody34 <woodruff1606@gmail.com>
2021-04-25 16:15:14 -0500

    stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-import * as components from './components/index'
+// import { AImageUploader } from './components'
+import * as components from './components'
+
+import './index.css'
 import { App } from 'vue'
 
-const install = (instance: App) => {
-    for (let componentKey in components) {
-        instance.use((components as any)[componentKey])
+
+// const install = (instance: App) => {
+//     for (let componentKey in components) {
+//         instance.component((components as any)[componentKey])
+//         // instance.use((components as any)[componentKey])
+//     }
+// }
+
+function install(Vue: App) {
+    for (const component in components) {
+        // @ts-expect-error
+        Vue.component(components[component].name, components[component])
     }
 }
 
-export default install
+
+export default { install }
 
 export * from './components'
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import babel from "vite-babel-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: [
+    {
+      find: '@',
+      replacement: path.resolve(__dirname, 'src'),
+    },
+  ],
   plugins: [
     vue(),
     vueJsx({
